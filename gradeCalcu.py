a3649e257beeb88f186a6d72e0d22fec2e83be26
EugeneDanielArago <eugenedanielarago@gmail.com>
2025-05-07 12:23:17 +0800

    Add files via upload

--- gradeCalcu.py
+++ gradeCalcu.py
@@ -1,35 +1,3 @@
-# def add(scores):
-#     sub = str(input("Enter the subject: "))
-#     score = int(input("Enter the score: "))
-#     scores.append(score)
-#     print("Score Added!")
-
-# def calcu(scores):
-#     average = sum(scores) / len(scores)
-#     print (f"Average Grade: {average}")
-
-
-# while True:
-
-#     print("\n------------------------")
-#     print("Student Grade Calculator")
-#     print("------------------------")
-#     print("1. Add Score")
-#     print("2. Calculate Average")
-#     print("3. Exit")
-#     choice = int(input("Enter your choice (1-3): "))
-
-#     if choice == 1:
-#         add()
-#     elif choice == 2:
-#         calcu()
-#     elif choice == 3:
-#         break
-#     else:
-#         print("Invalid choice! Please select a valid option (1-3).")
-
-
-
 def add(scores):
     sub = str(input("Enter the subject: "))
     score = int(input("Enter the score: "))
